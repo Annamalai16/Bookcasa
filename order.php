86e273267f5537dcf2444910f2ea7ecd13679fad
Annamalai <imemyselfanna@gmail.com>
2021-08-18 07:27:18 +0530

    redirect payment

--- order.php
+++ order.php
@@ -1,7 +1,4 @@
 <div style="text-align:center;">
 <h1 style="font-size:130%;margin-top:20%"> Click to Pay:</h1>
-<form style>
-    <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HmT4wk3kI337TW" async>
-    </script>
-</form>
+<form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HmUAjsoIP5AfQT" async> </script> </form>
 </div>
